6315c54a90f1c5476230829eedb90b53dfca8ff8
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2019-01-12 14:18:20 +0700

    fix bugs in define server host and image quality

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const io = require('socket.io')(server);
 
 const FPS = 30;
 const cap = new cv.VideoCapture(0);
-// cap.set(cv.CAP_PROP_FRAME_WIDTH, 300);
-// cap.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
+// cap.set(cv.CAP_PROP_FRAME_WIDTH, 640);
+// cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 setInterval(() => {
-    var flags = [cv.IMWRITE_JPEG_QUALITY, 80];
+    var flags = [cv.IMWRITE_JPEG_QUALITY, 40];
     const frame = cap.read();
     const image = cv.imencode('.jpg', frame, flags).toString('base64');
     io.emit('image', image);
